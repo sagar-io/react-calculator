d3a5b17c9dfbd93dc02d6ad06bb2c72f7576a047
Sagar Sharma <tech.sagarsh@gmail.com>
2022-07-12 15:44:02 +0530

    some neccessary modifications

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Button from "./components/Button";
 import "./style.css";
 import { useState, setState, useEffect } from "react";
 import calculations from "./calculations";
+import "./.env";
 
 const calcElementsArr = [ "AC", "⌫", ".", "+", 7, 8, 9, "-", 4, 5, 6, "x", 1, 2, 3, "÷", 0, "00", "FACT", "="];
 
@@ -11,30 +12,47 @@ export default function CalculatorApp() {
   const [operator, setOperator] = useState("");
   const [getFact, setGetFact] = useState(false);
   const [showFactTray, setShowFactTray] = useState(true);
-  const [fact, setFact] = useState('');
+  const [fact, setFact] = useState("");
   const [decreaseSize, setDecreaseSize] = useState(false);
 
   useEffect(() => {
-    fetch('http://numbersapi.com/random?json')
-    .then(response => response.json())
-    .then(data => {
-      setFact(data.text)
-      setShowFactTray(prevState => !prevState)
-    })
-  }, [getFact])
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.REACT_APP_KEY,
+        "X-RapidAPI-Host": process.env.REACT_APP_HOST,
+      },
+    };
+    let randomNum = Math.floor(Math.random() * 1000);
+
+    fetch(
+      `https://numbersapi.p.rapidapi.com/${randomNum}/math?fragment=true&json=true`,
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setFact(() => randomNum + " ➡ " + data.text);
+        setShowFactTray((prevState) => !prevState);
+      });
+  }, [getFact]);
 
   const buttonElements = calcElementsArr.map((btn, id) => (
-    <Button handleClick={handleBtnClick}
-            flipGetFact={flipGetFact}
-            key={id}
-            value={btn}
-            isShowFactTray = {showFactTray}
-         />
+    <Button
+      handleClick={handleBtnClick}
+      flipGetFact={flipGetFact}
+      key={id}
+      value={btn}
+      isShowFactTray={showFactTray}
+    />
   ));
 
   return (
     <div className="calc-container">
-      <div className={ decreaseSize ? "smallFontSize output-screen" : "output-screen"}>
+      <div
+        className={
+          decreaseSize ? "smallFontSize output-screen" : "output-screen"
+        }
+      >
         <div className="prev-operand">
           {prevOperand} {operator}
         </div>
@@ -42,91 +60,84 @@ export default function CalculatorApp() {
       </div>
 
       <div className="btn-container">{buttonElements}</div>
-      {showFactTray &&
-       <div className = "tray">
+      {showFactTray && (
+        <div className="tray">
           <p>"{fact}"</p>
-         </div>
-        }
+        </div>
+      )}
     </div>
   );
 
-  
   function flipGetFact() {
-    setGetFact(prevState => !prevState)
+    setGetFact((prevState) => !prevState);
   }
 
   function HandleOutputFontSize(bool) {
-    if(bool){
+    if (bool) {
       setDecreaseSize(true);
-    }else{
+    } else {
       setDecreaseSize(false);
     }
   }
 
-function handleBtnClick(e) {
-  const value = e.target.innerText;
+  function handleBtnClick(e) {
+    const value = e.target.innerText;
 
-  if (value === "AC") {
-    setCurrentOperand("");
-    setprevOperand("");
-    setOperator("");
-  }
-  
-  else if (value === "⌫") {
-      if(!currentOperand){
-          setCurrentOperand(prevOperand)
-          setprevOperand('')
-          setOperator('')
-      }else{
-           setCurrentOperand((prevState) => {
-      return prevState.substring(0, prevState.length - 1);
-    });}}
-  
-  else if(value === '-' && currentOperand === '-') {
-    return 
-  } 
-
-  else if ((value === "+" || value === "-" || value === "x" || value === "÷") && !(!currentOperand && value === '-' && operator != '-')){
-      if(!currentOperand && !prevOperand){
-          return
+    if (value === "AC") {
+      setCurrentOperand("");
+      setprevOperand("");
+      setOperator("");
+    } else if (value === "⌫") {
+      if (!currentOperand) {
+        setCurrentOperand(prevOperand);
+        setprevOperand("");
+        setOperator("");
+      } else {
+        setCurrentOperand((prevState) => {
+          return prevState.substring(0, prevState.length - 1);
+        });
       }
-        if (currentOperand && prevOperand && operator) {
-          let result = calculations(prevOperand, currentOperand, operator);
-          setCurrentOperand('');
-          setprevOperand(result.toFixed(2).toString());
-          setOperator(value);
-        } else {
-          setprevOperand((prevState) => prevState + currentOperand);
-          setCurrentOperand((prevState) => "");
-          setOperator(value);
-       }
-  }
-
-   else if ((value === "=" && operator  && currentOperand)){
-    let result = calculations(prevOperand, currentOperand, operator);
-    setCurrentOperand(result.toFixed(2).toString());
-    setprevOperand("");
-    setOperator("");
-  } 
-  
-  else if (value != "=") {
-    if((value === '.' && currentOperand.includes('.')))
-    {
-      return 
-    }
-    if(currentOperand.length > 12 || prevOperand.length > 12){
-       HandleOutputFontSize(true);
-    }else {
-      HandleOutputFontSize(false);
-    }
-       setCurrentOperand((prevState) =>{ 
-        if(value === '.' && !currentOperand){
-           return prevState + '0.'
-        }else if(currentOperand.length > 20){
-          return currentOperand
+    } else if (value === "-" && currentOperand === "-") {
+      return;
+    } else if (
+      (value === "+" || value === "-" || value === "x" || value === "÷") &&
+      !(!currentOperand && value === "-" && operator != "-")
+    ) {
+      if (!currentOperand && !prevOperand) {
+        return;
+      }
+      if (currentOperand && prevOperand && operator) {
+        let result = calculations(prevOperand, currentOperand, operator);
+        setCurrentOperand("");
+        setprevOperand(result.toFixed(2).toString());
+        setOperator(value);
+      } else {
+        setprevOperand((prevState) => prevState + currentOperand);
+        setCurrentOperand((prevState) => "");
+        setOperator(value);
+      }
+    } else if (value === "=" && operator && currentOperand) {
+      let result = calculations(prevOperand, currentOperand, operator);
+      setCurrentOperand(result.toFixed(2).toString());
+      setprevOperand("");
+      setOperator("");
+    } else if (value != "=") {
+      if (value === "." && currentOperand.includes(".")) {
+        return;
+      }
+      if (currentOperand.length > 12 || prevOperand.length > 12) {
+        HandleOutputFontSize(true);
+      } else {
+        HandleOutputFontSize(false);
+      }
+      setCurrentOperand((prevState) => {
+        if (value === "." && !currentOperand) {
+          return prevState + "0.";
+        } else if (currentOperand.length > 20) {
+          return currentOperand;
         }
-        return prevState + value
+        return prevState + value;
       });
+    }
   }
-}
 }
